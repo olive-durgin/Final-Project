ad52f9ff6c4f0cc515267841dd80e1a3e84d8298
olive-durgin <o.durgin@yahoo.com>
2024-11-03 16:58:10 -0600

    Game fight template

---
+++ src/fight-template.py
@@ -0,0 +1,34 @@
+def main():
+
+        strength = float(10)
+        health = float(100)
+        enemy_attack = float(5)
+        enemy_health = float(15)
+        new_health = health - enemy_attack
+
+        while enemy_health > 0:
+            attack_or_dodge = input("Type A to 'attack' or D to 'dodge': ")
+            attack_or_dodge = attack_or_dodge.capitalize()
+            if attack_or_dodge == "A":
+                enemy_health -= strength
+                print(f"The monster's health is now {enemy_health}!")
+                print("The monster attacks you.")
+                new_health -= enemy_attack
+                print(f"Your health is {new_health}")
+            elif attack_or_dodge == "D":
+                print("You attack too!")
+                enemy_health -= strength
+                print(f"The monster's health is now {enemy_health}!")
+            else:
+                new_health -= strength
+                print("You've been hit! What are you doing?")
+                print(f"Your health: {new_health}!")
+
+            if new_health <= 0:
+                print("You died...")
+                break
+            if enemy_health <= 0:
+                print("YOU WIN!")
+                break
+
+main()
