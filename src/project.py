cd54e9e731e57ce92b61b7b384daddf58972ce99
olive-durgin <o.durgin@yahoo.com>
2024-11-03 16:32:45 -0600

    Set up of the game's introduction

---
+++ src/project.py
@@ -0,0 +1,15 @@
+# Can input a name at the beginning.
+# Loading...
+# Hello. Please answer a few questions.
+    # adjective 01: ___.
+    # adjective 02: ___.
+    # number between 1-10 (unspecified): ___.
+# Player has the choice of picking between 3 different characters.
+    # Characters have a descirption using adjectives.
+    # Special skill.
+    # Strength.
+# Each player has a different ability, 
+# Third character has the input name, adjectives in description, and strength is 1-10.
+# Loading...
+# Welcomed by the UI.
+# 
