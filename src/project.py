cf92bf5cb51d6da5fc47be4700ece477070efa4e
olive-durgin <o.durgin@yahoo.com>
2024-11-03 19:39:20 -0600

    Added attack level and character templates

--- src/project.py
+++ src/project.py
@@ -20,6 +20,26 @@ def opening_scene():
     username = username.capitalize()
     adjective_01 = anim_input("Adjective 01: ")
     adjective_02 = anim_input("Adjective 02: ")
+
+    def attack_level():
+        strength = True
+        while strength:
+            anim_print(f"On a scale of one to ten, how strong are you?")
+            user_input = float(input("Choose wisely: "))
+            if user_input >= float(1) and user_input <= float(10):
+                strength = False
+            if user_input > float(10) or user_input <= float(0):
+                anim_print("This number is no good! Choose again!")
+        attack_level = user_input
+        return user_input
+            # INSERT ELIF (ie. elif strength contains non#) - add a response for when the player responds with any sort of non-alphabetical character. 
+            # if user_input != #numerical_value:
+            # running = True
+            # print("You call this a number...?")
+            # starts over when the player answers something that isn't a number.
+    attack_level()
+
+
     anim_print("Loading...", delay=0.135)
     time.sleep(1.5)
     anim_print(f"Hello, {username}!")
@@ -29,6 +49,44 @@ def opening_scene():
     anim_print("Your choices have consequences.")
     anim_print("So choose wisely...")
 
+    anim_print("Character: ANA")
+    time.sleep(1)
+    anim_print("Description: ___")
+    time.sleep(1)
+    anim_print("Health: 100")
+    time.sleep(1)
+    anim_print("Attack: 4")
+    time.sleep(1)
+    anim_print("Special Abilitiy: Discernment")
+    time.sleep(1)
+    anim_print("Weakness: ")
+    time.sleep(1.5)
+
+    anim_print("Character: LOUIS")
+    time.sleep(1)
+    anim_print("Description: ___")
+    time.sleep(1)
+    anim_print("Health: 100")
+    time.sleep(1)
+    anim_print("Attack: 4")
+    time.sleep(1)
+    anim_print("Special Abilitiy: Discernment")
+    time.sleep(1)
+    anim_print("Weakness: ")
+    time.sleep(1.5)
+
+    anim_print(f"Character: {username}")
+    time.sleep(1)
+    anim_print("Description: ___")
+    time.sleep(1)
+    anim_print("Health: 100")
+    time.sleep(1)
+    anim_print("Attack: 4")
+    time.sleep(1)
+    anim_print("Special Abilitiy: Discernment")
+    time.sleep(1)
+    anim_print("Weakness: ")
+    time.sleep(1.5)
 
 # Can input a name at the beginning.
 # Loading...
