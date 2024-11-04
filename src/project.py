a3a4b9393ad289b4c83b832abe6f99c3c07115cd
olive-durgin <o.durgin@yahoo.com>
2024-11-03 20:46:40 -0600

    Fixed crash error when non-numerical value is input into # loop

--- src/project.py
+++ src/project.py
@@ -24,30 +24,34 @@ def opening_scene():
     def attack_level():
         strength = True
         while strength:
-            anim_print(f"On a scale of one to ten, how strong are you?")
-            user_input = float(input("Choose wisely: "))
-            if user_input >= float(1) and user_input <= float(10):
-                strength = False
-            if user_input > float(10) or user_input <= float(0):
-                anim_print("This number is no good! Choose again!")
-        attack_level = user_input
-        return user_input
-            # INSERT ELIF (ie. elif strength contains non#) - add a response for when the player responds with any sort of non-alphabetical character. 
-            # if user_input != #numerical_value:
-            # running = True
-            # print("You call this a number...?")
-            # starts over when the player answers something that isn't a number.
-    attack_level()
+            try:
+                anim_print(f"Pick a number between one and ten.")
+                attack = float(input("Choose wisely: "))
+                if 1 <= attack <= 10:
+                    strength = False
+                else:
+                    anim_print("This is not what I asked for.")
+                    time.sleep(0.75)
+            except ValueError:
+                anim_print("Do you call that a number...?")
 
+        return attack
+
+    attack_level()
 
     anim_print("Loading...", delay=0.135)
     time.sleep(1.5)
     anim_print(f"Hello, {username}!")
-    anim_print("Welcome...")
+    anim_print("Welcome...", delay=0.14)
     anim_print("You feel... odd.")
+    time.sleep(1)
     anim_print("Who are you?")
+    time.sleep(1)
     anim_print("Your choices have consequences.")
+    time.sleep(0.5)
     anim_print("So choose wisely...")
+    time.sleep(1)
+    print()
 
     anim_print("Character: ANA")
     time.sleep(1)
@@ -61,6 +65,7 @@ def opening_scene():
     time.sleep(1)
     anim_print("Weakness: ")
     time.sleep(1.5)
+    print()
 
     anim_print("Character: LOUIS")
     time.sleep(1)
@@ -68,32 +73,28 @@ def opening_scene():
     time.sleep(1)
     anim_print("Health: 100")
     time.sleep(1)
-    anim_print("Attack: 4")
+    anim_print("Attack: 7")
     time.sleep(1)
     anim_print("Special Abilitiy: Discernment")
     time.sleep(1)
     anim_print("Weakness: ")
     time.sleep(1.5)
+    print()
 
+    attack = attack_level()
     anim_print(f"Character: {username}")
     time.sleep(1)
     anim_print("Description: ___")
     time.sleep(1)
     anim_print("Health: 100")
     time.sleep(1)
-    anim_print("Attack: 4")
+    anim_print(f"Attack: {attack}")
     time.sleep(1)
     anim_print("Special Abilitiy: Discernment")
     time.sleep(1)
     anim_print("Weakness: ")
     time.sleep(1.5)
 
-# Can input a name at the beginning.
-# Loading...
-# Hello. Please answer a few questions.
-    # adjective 01: ___.
-    # adjective 02: ___.
-    # number between 1-10 (unspecified): ___.
 # Player has the choice of picking between 3 different characters.
     # Characters have a descirption using adjectives.
     # Special skill.
