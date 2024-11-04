37288bab288ba1f93b047f5703c7ec8ae6cc7a79
olive-durgin <o.durgin@yahoo.com>
2024-11-04 09:16:34 -0600

    Created loop for choosing what character to play as

--- src/project.py
+++ src/project.py
@@ -16,16 +16,18 @@ def opening_scene():
     time.sleep(.07)
     anim_print("Loading...", delay=0.135)
     time.sleep(1.5)
-    username = anim_input("Input your name: ")
-    username = username.capitalize()
+    anim_print("Running info collection program...", delay=0.135)
+    time.sleep(1.5)
+    username = anim_input("Input your name: ").capitalize()
     adjective_01 = anim_input("Adjective 01: ")
     adjective_02 = anim_input("Adjective 02: ")
-
+    ability = anim_input("What are you good at? ")
+    weakness = anim_input("What scares you? ")
     strength = True
     while strength:
         try:
             anim_print(f"Pick a number between one and ten.")
-            attack = float(input("Choose wisely: "))
+            attack = float(anim_input("Choose wisely: "))
             if 1 <= attack <= 10:
                 strength = False
             else:
@@ -35,7 +37,7 @@ def opening_scene():
             anim_print("Do you call that a number...?")
     anim_print("Loading...", delay=0.135)
     time.sleep(1.5)
-    anim_print(f"Hello, {username}!")
+    print()
     anim_print("Welcome...", delay=0.14)
     anim_print("You feel... odd.")
     time.sleep(1)
@@ -49,52 +51,64 @@ def opening_scene():
 
     anim_print("Character: ANA")
     time.sleep(1)
-    anim_print("Description: ___")
+    anim_print("Description: Ana is happy and kinda brave. There's not much else to say.")
     time.sleep(1)
     anim_print("Health: 100")
     anim_print("Attack: 4")
-    anim_print("Special Abilitiy: Discernment")
-    anim_print("Weakness: ")
+    anim_print("Special Ability: Discernment")
+    anim_print("Weakness: Spiders")
     time.sleep(1.5)
     print()
 
     anim_print("Character: LOUIS")
     time.sleep(1)
-    anim_print("Description: ___")
+    anim_print("Description: Louis is scared and kinda alone. There's not much else to say.")
     time.sleep(1)
     anim_print("Health: 100")
-    time.sleep(1)
     anim_print("Attack: 7")
-    time.sleep(1)
-    anim_print("Special Abilitiy: Discernment")
-    time.sleep(1)
-    anim_print("Weakness: ")
+    anim_print("Special Ability: Discernment")
+    anim_print("Weakness: Mice")
     time.sleep(1.5)
     print()
 
-    anim_print(f"Character: {username}")
+    anim_print(f"Character: {username.upper()}")
     time.sleep(1)
-    anim_print(f"Description: {username} is {adjective_01} and kinda {adjective_02}. There's not much else to say.")
+    anim_print(f"Description: {username.title()} is {adjective_01} and kinda {adjective_02}. There's not much else to say.")
     time.sleep(1)
     anim_print("Health: 100")
-    time.sleep(1)
     anim_print(f"Attack: {attack}")
-    time.sleep(1)
-    anim_print("Special Abilitiy: Discernment")
-    time.sleep(1)
-    anim_print("Weakness: ")
-    time.sleep(1.5)
+    anim_print(f"Special Ability: {ability.capitalize()}")
+    anim_print(f"Weakness: {weakness.capitalize()}")
+    time.sleep(2)
+    print()
+    correct_choice = True
+    correct_choice = False
+    while not correct_choice:
+            anim_print("Who are you?")
+            character_choice = anim_input("Choose wisely: ").capitalize()
+            if character_choice == "Ana" or character_choice == "Louis" or character_choice == username:
+                correct_choice = True
+            else:
+                anim_print("This is not what I asked for.")
 
+    if character_choice == "Ana":
+        anim_print("Okay...")
+        anim_print("Creating scenario...", delay=0.135)
+        time.sleep(2)
+        anim_print("Loading...", delay=0.135)
 
-# Player has the choice of picking between 3 different characters.
-    # Characters have a descirption using adjectives.
-    # Special skill.
-    # Strength.
-# Each player has a different ability, 
-# Third character has the input name, adjectives in description, and strength is 1-10.
-# Loading...
-# Welcomed by the UI.
-# 
+    if character_choice == "Louis":
+        anim_print("Interesting...")
+        anim_print("Creating scenario...", delay=0.135)
+        time.sleep(2)
+        anim_print("Loading...", delay=0.135)
+
+    if character_choice == username:
+        anim_print("I see...")
+        anim_print("Creating scenario...", delay=0.135)
+        time.sleep(2)
+        anim_print("Loading...", delay=0.135)
+
 
 if __name__ == "__main__":
     opening_scene()
