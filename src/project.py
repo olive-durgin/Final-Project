194cfbe4fee942e22e67b19b06ec5a9b72de54e2
olive-durgin <o.durgin@yahoo.com>
2024-11-03 19:35:59 -0600

    Animated inputs and added pauses

--- src/project.py
+++ src/project.py
@@ -1,14 +1,33 @@
 import time
 
-def anim_print(AIUI_speech):
-
-    for character in AIUI_speech:
-        print(character, end = "", flush = True)
-        time.sleep(0.052)
+def anim_print(UI_text, delay=0.052):
+    for character in UI_text:
+        print(character, end="", flush=True)
+        time.sleep(delay)
     print()
 
+def anim_input(prompt):
+    for character in prompt:
+        print(character, end="", flush=True)
+        time.sleep(0.062)
+    return input()
+
 def opening_scene():
-    anim_print("Loading...")
+    time.sleep(.07)
+    anim_print("Loading...", delay=0.135)
+    time.sleep(1.5)
+    username = anim_input("Input your name: ")
+    username = username.capitalize()
+    adjective_01 = anim_input("Adjective 01: ")
+    adjective_02 = anim_input("Adjective 02: ")
+    anim_print("Loading...", delay=0.135)
+    time.sleep(1.5)
+    anim_print(f"Hello, {username}!")
+    anim_print("Welcome...")
+    anim_print("You feel... odd.")
+    anim_print("Who are you?")
+    anim_print("Your choices have consequences.")
+    anim_print("So choose wisely...")
 
 
 # Can input a name at the beginning.
