5f01e33243dd67193708892fe59f1d05a738cc08
olive-durgin <o.durgin@yahoo.com>
2024-11-04 20:48:10 -0600

    Moved first enemy attack outside of loop and added dialgoue

--- src/fight_easy.py
+++ src/fight_easy.py
@@ -1,3 +1,5 @@
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame, time, csv, project
 pygame.init()
 sound = pygame.mixer.Sound(r'sounds\easy_fight.mp3')
@@ -23,76 +25,95 @@ def easy_fight(strength):
         anim_print("You've been ambushed by a low-level enemy!")
         anim_print("The rules are simple.")
         anim_print("Try not to get hit.")
-        anim_print("Right after the enemy attack, you have a window of opportunity to fight back.")
+        anim_print("Right after the enemy attacks, you have a window of opportunity to fight back.")
         anim_print("Don't miss this chance.")
         anim_print("Be strategic about this, because sometimes, the enemy can be smarter than it looks.")
         anim_print("You shouldn't have any issues with this particular enemy though.")
         anim_print("Feel free to take your time.")
         anim_print("You won't need an inventory for this fight either.")
-        strength = 10
+        anim_print("Unfortunately, since this is your first fight, you're nervous.")
+        anim_print("So, you're attacks aren't as strong.")
+        anim_print("Your attack is set to 5")
+        strength = float(5)
         health = float(100)
         enemy_attack = float(5)
         enemy_health = float(15)
         new_health = health - enemy_attack
         # access inventory
+        anim_print("The enemy in front of you boasts sharp teeth!")
+        anim_print("The enemy moves slowly but still manages to hit you!")
+        new_health -= enemy_attack
+        anim_print(f"Your health decreases by {enemy_attack}!")
+        anim_print(f"Your health dropped to {new_health}!")
+        anim_print("Now is your chance!")
+        anim_print("Show that monster what you got before it gets a chance to attack again!")
         while enemy_health > 0:
-            anim_print("The enemy in front of you boasts sharp teeth!")
-            anim_print("The enemy moves slowly but still manages to hit you!")
-            new_health -= enemy_attack
-            anim_print(f"Your health decreases by {enemy_attack}!")
-            anim_print(f"Your health dropped to {new_health}!")
-            anim_print("Now is your chance!")
-            anim_print("Show that monster what you got before it gets a chance to attack again!")
-
             attack_or_dodge = input("Type A to 'attack' or D to 'dodge': ")
             attack_or_dodge = attack_or_dodge.capitalize()
             if attack_or_dodge == "A":
                 hero_attack.play()
                 enemy_health -= strength
-                print(f"The monster's health is now {enemy_health}!")
+                anim_print(f"The monster's health is now {enemy_health}!")
                 if enemy_health <= 0:
                     sound.stop()
-                    print("YOU WIN!")                   
+                    anim_print("YOU WIN!")                   
                     win_sound.play()
                     time.sleep(5)
                     win_sound.stop()
                     break
                 if new_health <= 0:
-                    print("You died...")
+                    anim_print("You died...")
+                    time.sleep(4)
+                    lose_sound.stop()
+                    anim_print("Overriding program...")
+                    time.sleep(2)
+                    anim_print("Resetting...")
                     lose_sound.play()
                     break
-                print("The monster attacks you.")
+                anim_print("The monster attacks you.")
                 monster_attack.play()
                 new_health -= enemy_attack
-                print(f"Your health is {new_health}")
+                anim_print(f"Your health is {new_health}")
             elif attack_or_dodge == "D":
-                print("You attack too!")
+                anim_print("You successfully dodged the monster's attack!")
+                anim_print("Be careful! Next time you might not be so lucky!")
+                anim_print("You're are able to attack while the monster picks itself up off the ground!")
                 hero_attack.play()
                 enemy_health -= strength
-                print(f"The monster's health is now {enemy_health}!")
+                anim_print(f"The monster's health is now {enemy_health}!")
                 if enemy_health <= 0:
                     sound.stop()
-                    print("YOU WIN!")
+                    anim_print("YOU WIN!")
                     win_sound.play()
                     time.sleep(5)
                     win_sound.stop()
                     break
                 if new_health <= 0:
-                    print("You died...")
                     lose_sound.play()
+                    anim_print("You died...")
+                    time.sleep(4)
+                    lose_sound.stop()
+                    anim_print("Overriding program...")
+                    time.sleep(2)
+                    anim_print("Resetting...")
                     break
             else:
                 new_health -= strength
                 monster_attack.play()
-                print("You've been hit! What are you doing?")
-                print(f"Your health: {new_health}!")
+                anim_print("You've been hit! What are you doing?")
+                anim_print(f"Your health: {new_health}!")
             if new_health <= 0:
                 print("You died...")
                 lose_sound.play()
+                time.sleep(4)
+                lose_sound.stop()
+                anim_print("Overriding program...")
+                time.sleep(2)
+                anim_print("Resetting...")
                 break
             if enemy_health <= 0:
                 sound.stop()
-                print("YOU WIN!")
+                anim_print("YOU WIN!")
                 win_sound.play()
                 time.sleep(5)
                 win_sound.stop()
@@ -101,4 +122,4 @@ def easy_fight(strength):
         anim_print("Loading...", delay=0.135)
         time.sleep(3)
 
-easy_fight()
+easy_fight(100)
