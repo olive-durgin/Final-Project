f1aad675b43b4604d8b15e3c0b57cfdc1eb0e119
olive-durgin <o.durgin@yahoo.com>
2024-11-18 19:33:18 -0600

    Simplified code for fight

--- src/first_fight.py
+++ src/first_fight.py
@@ -20,113 +20,50 @@ def anim_input(prompt):
         time.sleep(0.062)
     return input()
 
-def easy_fight(strength):
-        sound.set_volume(3)
-        sound.play(loops=-1)
-        anim_print("You've been ambushed by a mutated rat!")
-        anim_print("The rules are simple.")
-        anim_print("Try not to get hit.")
-        anim_print("Right after the enemy attacks, you have a window of opportunity to fight back.")
-        anim_print("Don't miss this chance.")
-        anim_print("Be strategic about this, because sometimes, the enemy can be smarter than it looks.")
-        anim_print("You shouldn't have any issues with this particular enemy though.")
-        anim_print("Feel free to take your time.")
-        anim_print("There is no consiquence for waiting to long.")
-        anim_print("You won't need an inventory for this fight either.")
-        anim_print("Unfortunately, since this is your first fight, you're nervous.")
-        anim_print("So, you're attacks aren't as strong.")
-        anim_print("Your attack is set to 5.")
-        anim_print("And your health is 100!")
+def easy_fight():
+
+    game_is_running = True
+    while game_is_running:
+        sound.play(-1)
         strength = float(5)
-        health = float(100)
-        enemy_attack = float(5)
+        new_health = float(100)
+        enemy_attack = float(25)
         enemy_health = float(15)
-        new_health = health - enemy_attack
-        # access inventory
-        anim_print("The grotesque rat in front of you boasts its sharp teeth!")
-        anim_print("The rat moves slowly but still manages to hit you!")
-        monster_attack.play()
-        health -= enemy_attack
-        anim_print(f"Your health decreases by {enemy_attack}!")
-        anim_print(f"Your health dropped to {new_health}!")
-        anim_print("Now is your chance!")
-        anim_print("Show that rat what you got before it gets a chance to attack again!")
         while enemy_health > 0:
-            attack_or_dodge = input("Type A to 'attack' or D to 'dodge': ")
-            attack_or_dodge = attack_or_dodge.capitalize()
+            attack_or_dodge = anim_input("Type A to 'attack' or D to 'dodge': ").capitalize()
             if attack_or_dodge == "A":
-                hero_attack.play()
                 enemy_health -= strength
                 anim_print(f"The rat's health is now {enemy_health}!")
                 if enemy_health <= 0:
                     anim_print("YOU WIN!")
-                    win_sound.set_volume(2)              
-                    win_sound.play()
-                    time.sleep(3)
-                    break
-                if new_health <= 0:
-                    enemy_health = True
-                    anim_print("You died...")
                     sound.stop()
-                    time.sleep(4)
-                    anim_print("Overriding program...")
-                    time.sleep(2)
-                    anim_print("Resetting...")
+                    game_is_running = False
                     break
-                anim_print("The rat lunges at you!")
-                monster_attack.play()
                 new_health -= enemy_attack
-                anim_print(f"Your health is {new_health}!")
+                anim_print(f"Your health is now {new_health}!")
+                if new_health <= 0:
+                    anim_print("You died...")
+                    sound.stop()
+                    game_is_running = False
+                    break
             elif attack_or_dodge == "D":
-                anim_print("The rat lunges at you with the intention to kill!")
-                anim_print("But you successfully dodged the rat's attack!")
-                anim_print("Be careful! Next time you might not be so lucky!")
-                anim_print("The rat falls to the ground after missing, and you're are able to attack while the rat picks itself up off the ground!")
-                hero_attack.play()
                 enemy_health -= strength
                 anim_print(f"The rat's health is now {enemy_health}!")
                 if enemy_health <= 0:
-                    sound.stop()
+                    game_is_running = False
                     anim_print("YOU WIN!")
-                    win_sound.set_volume(2)
-                    win_sound.play()
-                    time.sleep(3)
-                    break
-                if new_health <= 0:
-                    enemy_health = True
-                    lose_sound.play()
-                    sound.stop()
-                    anim_print("You died...")
-                    time.sleep(4)
-                    anim_print("Overriding program...")
-                    time.sleep(2)
-                    anim_print("Resetting...")
                     break
             else:
-                new_health -= strength
-                monster_attack.play()
+                new_health -= enemy_attack
                 anim_print("You've been hit! What are you doing?")
-                anim_print(f"Your health is {new_health}!")
-            if new_health <= 0:
-                enemy_health = True
-                print("You died...")
-                lose_sound.play()
-                sound.stop()
-                time.sleep(4)
-                anim_print("Overriding program...")
-                time.sleep(2)
-                anim_print("Resetting...")
-            if enemy_health <= 0:
-                sound.stop()
-                anim_print("YOU WIN!")
-                win_sound.set_volume(2)
-                win_sound.play()
-                time.sleep(3)
-                win_sound.stop()
-                break
-        time.sleep(2)
-        sound.stop()
-        anim_print("Loading...", delay=0.135)
-        time.sleep(3)
-
-easy_fight(100)
+                anim_print(f"Your health is now {new_health}!")
+                if new_health <= 0:
+                    anim_print("You died...")
+                    sound.stop()
+                    game_is_running = True
+                    break
+    time.sleep(2) 
+    sound.stop()
+    anim_print("Loading...", delay=0.135)
+    time.sleep(3)
+easy_fight()
